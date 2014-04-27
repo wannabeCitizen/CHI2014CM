9fe9d898fa4babe662a4f3a2d13c20918e2a2082
danyelf <danyelf@acm.org>
2014-04-27 16:29:47 -0400

    Added d3 javascipt  code for sparklines

--- Website/app.js
+++ Website/app.js
@@ -1,27 +1,47 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () {
-            return _this.span.innerHTML = new Date().toUTCString();
-        }, 500);
-    };
+var width = 100;
+var height = 25;
+var x = d3.scale.linear().range([0, width - 2]);
+var y = d3.scale.linear().range([height - 4, 0]);
+var parseDate = d3.time.format("%b %d, %Y").parse;
+var line = d3.svg.line().interpolate("basis").x(function (d) {
+    return x(d.date);
+}).y(function (d) {
+    return y(d.close);
+});
 
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-})();
+function sparkline(elemId, data) {
+    x.domain(d3.extent(data, function (d) {
+        return d.date;
+    }));
+    y.domain(d3.extent(data, function (d) {
+        return d.close;
+    }));
+
+    var svg = d3.select(elemId).append('svg').attr('width', width).attr('height', height).append('g').attr('transform', 'translate(0, 2)');
+    svg.append('path').datum(data).attr('class', 'sparkline').attr('d', line);
+    svg.append('circle').attr('class', 'sparkcircle').attr('cx', x(data[0].date)).attr('cy', y(data[0].close)).attr('r', 1.5);
+}
+
+function randomData() {
+    var data = [];
+    for (var i = 0; i < 15; i++) {
+        data.push({ date: i, close: Math.random() });
+    }
+    return data;
+}
+
+function drawBar(x) {
+    var s = Math.round(Math.random() * 100) + ' percentile';
+    x.textContent = s;
+}
 
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    sparkline('#s1', randomData());
+    sparkline('#s2', randomData());
+    sparkline('#s3', randomData());
+
+    d3.selectAll('.percentile ').each(function (s) {
+        drawBar(this);
+    });
 };
 //# sourceMappingURL=app.js.map
--- Website/app.ts
+++ Website/app.ts
@@ -1,28 +1,54 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+var width = 100;
+var height = 25;
+var x = d3.scale.linear().range([0, width - 2]);
+var y = d3.scale.linear().range([height - 4, 0]);
+var parseDate = d3.time.format("%b %d, %Y").parse;
+var line = d3.svg.line()
+    .interpolate("basis")
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.close); });
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+function sparkline(elemId, data) {
+    x.domain(d3.extent(data, function (d) { return d.date; }));
+    y.domain(d3.extent(data, function (d) { return d.close; }));
+
+    var svg = d3.select(elemId)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(0, 2)');
+    svg.append('path')
+        .datum(data)
+        .attr('class', 'sparkline')
+        .attr('d', line);
+    svg.append('circle')
+        .attr('class', 'sparkcircle')
+        .attr('cx', x(data[0].date))
+        .attr('cy', y(data[0].close))
+        .attr('r', 1.5);
+}
+
+function randomData() {
+    var data = [];
+    for (var i = 0; i < 15; i++) {
+        data.push({ date: i, close: Math.random() });
     }
+    return data;
+}
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
+function drawBar(x) {
+    
+    var s = Math.round(Math.random() * 100) + ' percentile';
+    x.textContent = s;
 }
 
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    sparkline('#s1', randomData());
+    sparkline('#s2', randomData());
+    sparkline('#s3', randomData());
+
+    d3.selectAll('.percentile ').each(function (s) { drawBar(this) });
+
+
 };
