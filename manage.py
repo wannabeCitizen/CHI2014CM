dbf44952bea4ca6bb86bc4fc244776ae5410442d
wannabeCitizen <doorsofskirpan@gmail.com>
2014-04-27 13:33:29 -0400

    ready hopefully

---
+++ manage.py
@@ -0,0 +1,17 @@
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flask.ext.script import Manager, Server
+from CHI2014CM import app
+
+manager = Manager(app)
+
+manager.add_command("runserver", Server(
+	use_debugger = True,
+	use_reloader = True,
+	port = 5000,
+	host = '0.0.0.0')
+)
+
+if __name__ == "__main__":
+	manager.run()
---
+++ views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, request, redirect, render_template, url_for
+from flask.views import MethodView
+from slugify import slugify
+
+from flask.ext.mongoengine.wtf import model_form
+from models import *
+
+user = Blueprint('chi_folk', __name__, template_folder='templates')
+
+class Search(MethodView):
+
+	def get(self):
+		name = request.args.get('name', '')
+
+class Home(MethodView):
+
+	def get(self):
+		return render_template('chi_folk/home.html')
+
+user.add_url_rule('/', view_func=Home.as_view('home'))
